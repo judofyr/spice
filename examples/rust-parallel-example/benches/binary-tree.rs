bc39f6c90497ddac3382f8e973caca9dfa478d23
Godfrey Chan <godfreykfc@gmail.com>
2024-08-13 02:15:46 -0700

    Cleanup benchmark script
    The main thing is that the purpose of `bench_with_input` is to
    automatically wrap the input in `black_box`, so it was needlessly
    double-wrapped.

--- examples/rust-parallel-example/benches/binary-tree.rs
+++ examples/rust-parallel-example/benches/binary-tree.rs
@@ -1,17 +1,22 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
-use parallel_example::{sum, sum_rayon, make_balanced_tree};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use parallel_example::{make_balanced_tree, sum, sum_rayon};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    for &n in [1000, 100_000_000].iter() {
+    for n in [1000, 100_000_000] {
         let mut group = c.benchmark_group(format!("tree-sum-{}", n));
         group.sample_size(50);
         let root = make_balanced_tree(1, n);
-        group.bench_with_input(BenchmarkId::new("Baseline", 1), &1,
-            |b, _i| b.iter(|| sum(black_box(&root))));
-        for &num_threads in [1, 2, 4, 8, 16, 32].iter() {
-            let pool = rayon::ThreadPoolBuilder::new().num_threads(num_threads).build().unwrap();
-            group.bench_with_input(BenchmarkId::new("Rayon", num_threads), &num_threads,
-                |b, _i| b.iter(|| sum_rayon(&pool, black_box(&root))));
+        group.bench_with_input(BenchmarkId::new("Baseline", 1), &root, |b, root| {
+            b.iter(|| sum(root))
+        });
+        for num_threads in [1, 2, 4, 8, 16, 32] {
+            let pool = rayon::ThreadPoolBuilder::new()
+                .num_threads(num_threads)
+                .build()
+                .unwrap();
+            group.bench_with_input(BenchmarkId::new("Rayon", num_threads), &root, |b, root| {
+                b.iter(|| sum_rayon(&pool, root))
+            });
         }
     }
 }
