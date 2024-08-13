589c90df1eb68535bf1f48bdae7cdf45e95d7c1f
Godfrey Chan <godfreykfc@gmail.com>
2024-08-13 01:13:49 -0700

    Fix clippy warnings
    These are just linter warnings about unidiomatic/unnecessary code,
    should be pretty harmless to apply.

--- examples/rust-parallel-example/src/lib.rs
+++ examples/rust-parallel-example/src/lib.rs
@@ -5,12 +5,12 @@ pub struct Node<T> {
 }
 
 pub fn make_balanced_tree(from: i64, to: i64) -> Node<i64> {
-    let value = from + (to-from)/2;
-    return Node {
-        value: value,
+    let value = from + (to - from) / 2;
+    Node {
+        value,
         left: (value > from).then(|| Box::new(make_balanced_tree(from, value - 1))),
         right: (value < to).then(|| Box::new(make_balanced_tree(value + 1, to))),
-    };
+    }
 }
 
 pub fn sum(node: &Node<i64>) -> i64 {
@@ -21,15 +21,13 @@ pub fn sum(node: &Node<i64>) -> i64 {
     if let Some(child) = &node.right {
         result += sum(child);
     }
-    return result;
+    result
 }
 
 pub fn sum_rayon(pool: &rayon::ThreadPool, node: &Node<i64>) -> i64 {
     if let (Some(left), Some(right)) = (&node.left, &node.right) {
-        let (left_result, right_result) = pool.join(
-            || sum_rayon(pool, left),
-            || sum_rayon(pool, right),
-        );
+        let (left_result, right_result) =
+            pool.join(|| sum_rayon(pool, left), || sum_rayon(pool, right));
         return node.value + left_result + right_result;
     }
 
@@ -40,5 +38,5 @@ pub fn sum_rayon(pool: &rayon::ThreadPool, node: &Node<i64>) -> i64 {
     if let Some(child) = &node.right {
         result += sum_rayon(pool, child);
     }
-    return result;
+    result
 }
